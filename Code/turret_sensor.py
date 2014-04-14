60f1ef3f1db46cebd5ce51c7842e4e2551ed96d6
zewwo <azn_goku_91@hotmail.com>
2014-04-14 17:21:12 -0600

    Update turret_sensor.py

--- Code/turret_sensor.py
+++ Code/turret_sensor.py
@@ -6,6 +6,7 @@
 # ---------------------------------
 # Author	Date		Comment
 # Nathan	03.15.14	Created.
+# Kevin 	04.14.14	Fixed a bug and remove unnecessary code
 #######################################################
 ''
 
@@ -56,53 +57,37 @@ myinput = ""
 delay = 0.1
 	
 def up():	
-	if BrickPi.Sensor[tiltMax] == 0 and buttons & cwiid.BTN_RIGHT:				# keep going up until it hits the tilt sensor at max or until the user lets go of the dpad's right button
+	if BrickPi.Sensor[tiltMax] == 0:					# go up until it hits the tilt sensor at max or until the user lets go of the dpad's right button
 		BrickPi.MotorSpeed[tiltMotor] = -130
 		BrickPiUpdateValues()
 
-	if BrickPi.Sensor[tiltMax] == 0 and senseButs & cwiid.BTN_RIGHT:
-		BrickPi.MotorSpeed[tiltMotor] = -130
-		BrickPiUpdateValues()
 
 def down():
-	if BrickPi.Sensor[tiltMin] == 0 and buttons & cwiid.BTN_LEFT:				# keep going up until it hits the tilt sensor at max or until the user lets go of the dpad's left button
+	if BrickPi.Sensor[tiltMin] == 0:					# go down until it hits the tilt sensor at max or until the user lets go of the dpad's left button
 		BrickPi.MotorSpeed[tiltMotor] = 30		
 		BrickPiUpdateValues()
-	if BrickPi.Sensor[tiltMin] == 0 and senseButs & cwiid.BTN_LEFT:				# keep going up until it hits the tilt sensor at max or until the user lets go of the dpad's left button
-		BrickPi.MotorSpeed[tiltMotor] = 30		
-		BrickPiUpdateValues()
+
 	
 
 def shoot():
-	if buttons == 1:
-		BrickPi.MotorSpeed[shootMotor] = 255							# turn the motor on to shoot until the user lets go of the 2 button
-		BrickPiUpdateValues()
-	elif io.input(pir_pin):
-		BrickPi.MotorSpeed[shootMotor] = 255							# turn the motor on to shoot until the user lets go of the 2 button
-		BrickPiUpdateValues()
+	BrickPi.MotorSpeed[shootMotor] = 255					# turn the motor on to shoot until the user lets go of the 2 button
+	BrickPiUpdateValues()
 
 
-def turnLeft():
-	if buttons & cwiid.BTN_UP:													# turn left until the user lets go of the dpad's left button
-		BrickPi.MotorSpeed[rotateMotor] = -130
-		BrickPiUpdateValues()	
-	if senseButs & cwiid.BTN_UP:													# turn left until the user lets go of the dpad's left button
-		BrickPi.MotorSpeed[rotateMotor] = -130
-		BrickPiUpdateValues()
+def turnLeft():									# turn left until the user lets go of the dpad's left button
+	BrickPi.MotorSpeed[rotateMotor] = -130
+	BrickPiUpdateValues()	
 	
-def turnRight():
-	if buttons & cwiid.BTN_DOWN:													# turn left until the user lets go of the dpad's right button
-		BrickPi.MotorSpeed[rotateMotor] = 130
-		BrickPiUpdateValues()	
-	if senseButs & cwiid.BTN_DOWN:													# turn left until the user lets go of the dpad's right button
-		BrickPi.MotorSpeed[rotateMotor] = 130	
-		BrickPiUpdateValues()
+def turnRight():								# turn right until the user lets go of the dpad's right button
+	BrickPi.MotorSpeed[rotateMotor] = 130
+	BrickPiUpdateValues()	
+
 
 
 print "\nPress 1 + 2 on your Wii Remote to connect to the turret..."
 time.sleep(1)
 
-while True:											# keep looping until there is a wii remote connected
+while True:									# keep looping until there is a wii remote connected
 	try:
 		wii = cwiid.Wiimote()												# attempt to connect the wii remote to the script
 		break
@@ -121,13 +106,18 @@ print "DPAD LEFT = Turn Left\tDPAD RIGHT = Turn Right"
 print "2 = Shoot\t\tHOME = Go to Auto Shoot Mode!"
 print "\nPress + and - to exit the script"
 	
-wii.rpt_mode = cwiid.RPT_BTN											# set the mode to report button presses
+wii.rpt_mode = cwiid.RPT_BTN							# set the mode to report button presses
 
-while True:																# exit code when the user press + and - on the remote	
-	buttons = wii.state['buttons']										# get current remote states
+while True:														# exit code when the user press + and - on the remote	
+	buttons = wii.state['buttons']						# get current remote states
 	senseButs = 0
-
-	if buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0:					# check if the user pressed + and - simultaneously
+	
+	aim = False								# bools representing if the motor is on or not
+	shot = False
+	turn = False
+	
+	
+	if buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0:			# check if the user pressed + and - simultaneously
 		print "Exiting script and connection..."
 		wii.rumble = 1													# rumble for 1s to indicate the user that it is exiting the script
 		time.sleep(1)
@@ -135,50 +125,67 @@ while True:																# exit code when the user press + and - on the remote
 		exit(wii)
 		quit()									
 	
-	if buttons & cwiid.BTN_RIGHT:										# perform function calls according to what the state of the wii remote is
+	if buttons & cwiid.BTN_RIGHT:						# perform function calls according to what the state of the wii remote is
+		aim = True
 		up()
 	elif buttons & cwiid.BTN_LEFT:										
+		aim = True
 		down()
-	elif buttons & cwiid.BTN_UP:										
+	if buttons & cwiid.BTN_UP:										
+		turn = True
 		turnLeft()	
 	elif buttons & cwiid.BTN_DOWN:										
+		turn = True
 		turnRight()
-	elif buttons & cwiid.BTN_2:
+	if buttons & cwiid.BTN_2:
+		shot = True
 		shoot()
-	elif buttons & cwiid.BTN_HOME:
+	if buttons & cwiid.BTN_HOME:
 		wii.led = 2
 		print "\n\n\t\tAuto Shoot Mode!"
 		print "(Uses PIR Sensor + Manual Shooting is disabled!)"
 		print "\t\tSame Controls"
 		print "\t1 = Go back to Remote Mode!"
 		while True:
-			senseButs = wii.state['buttons']			
-
-			if senseButs & cwiid.BTN_1:						# go back to normal mode if user pressed 1
+			sensorWii = wii.state['buttons']			
+			
+			senseTurn = False
+			senseAim = False
+			
+			if senseButs & cwiid.BTN_1:				# go back to normal mode if user pressed 1
 				wii.rumble = 1
 				time.sleep(1)
 				wii.rumble = 0
 				wii.led = 1
 				break
 		
-			if senseButs & cwiid.BTN_RIGHT:
+			if sensorWii & cwiid.BTN_RIGHT:
+				senseAim = True
 				up()
-			elif senseButs & cwiid.BTN_LEFT:
+			elif sensorWii & cwiid.BTN_LEFT:
+				senseAim = True
 				down()
-			elif senseButs & cwiid.BTN_UP:
+			if sensorWii & cwiid.BTN_UP:
+				senseTurn = False
 				turnLeft()
-			elif senseButs & cwiid.BTN_DOWN:
+			elif sensorWii & cwiid.BTN_DOWN:
+				senseTurn = True
 				turnRight()
-			if senseButs == 0:
-				BrickPi.MotorSpeed[tiltMotor] = 0	
-				BrickPi.MotorSpeed[rotateMotor] = 0	
-				BrickPiUpdateValues()
-
-			if io.input(pir_pin):							# shoot for 4s if the sensor activates
+				
+			if io.input(pir_pin):					# shoot for 4s if the sensor activates
 				shoot()
 			else:
 				BrickPi.MotorSpeed[shootMotor] = 0
 				BrickPiUpdateValues()	
+				
+				
+			if !senseAim:	
+				BrickPi.MotorSpeed[tiltMotor] = 0
+				BrickPiUpdateValues()
+			if !senseTurn:
+				BrickPi.MotorSpeed[rotateMotor] = 0
+				BrickPiUpdateValues()
+				
 			time.sleep(delay)
 		print "\n\t\tRemote Mode!"
 		print "Controls ( TURN WII REMOTE HORIZONTALLY ):"
@@ -187,10 +194,14 @@ while True:																# exit code when the user press + and - on the remote
 		print "2 = Shoot\t\tHOME = Go to Auto Shoot Mode!"
 		print "\nPress + and - to exit the script"
 	
-	if buttons == 0:													# if there are no buttons that are pressed turn all motors off
-		BrickPi.MotorSpeed[shootMotor] = 0
-		BrickPi.MotorSpeed[tiltMotor] = 0	
-		BrickPi.MotorSpeed[rotateMotor] = 0	
+	if !aim:								# if any of the lock bools is false, turn off the respective motors
+		BrickPi.MotorSpeed[tiltMotor] = 0
 		BrickPiUpdateValues()
-	
+	if !shot:
+		BrickPi.MotorSpeed[shootMotor] = 0
+		BrickPiUpdateValues()	
+	if !turn:
+		BrickPi.MotorSpeed[rotateMotor] = 0
+		BrickPiUpdateValues()
+
 	time.sleep(delay)
